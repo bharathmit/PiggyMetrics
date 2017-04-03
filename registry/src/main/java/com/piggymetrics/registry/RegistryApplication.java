5e8ba2388908ab847caadee4fe766158e0e7bea3
bharath <bharathkumar.feb14@gmail.com>
2017-04-03 11:55:50 +0530

    registy file change

--- registry/src/main/java/com/piggymetrics/registry/RegistryApplication.java
+++ registry/src/main/java/com/piggymetrics/registry/RegistryApplication.java
@@ -1,8 +1,12 @@
 package com.piggymetrics.registry;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,4 +15,17 @@ public class RegistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
 	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message.greeting}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
 }
